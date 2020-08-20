eef31328e3e56091fcd0b5abe239533d5242b72a
hodayash <hodayash@gmail.com>
2020-08-20 16:46:13 +0300

    minor fixes

--- animals_parser.py
+++ animals_parser.py
@@ -4,19 +4,20 @@ and the animals which belong to each one.
 
 It dBoes so by scraping a web page and extracting relevant data.
 
-Requirments: Python 3.6 +
+Requirements: Python 3.6+
 Packages: Pandas, requests, bs4 (BeautifulSoup)
 
 This file can also be imported as a module in order to use the animal grabber.
 """
+
 import pandas as pd
 from html_parser import HTMLTableParser
 import consts
 
 def merge_animals_tables(tables):
     """
-    gets list of tables as DFs.
     merges the animals tables to one table of animal + collateral adjectives.
+    :param tables: list of DataFrames.
     :return: pd.DataFrame representing all animals' data
     """
     relevant = [table for table in tables if consts.ANIMAL_COL in table.columns.to_list()]
@@ -24,7 +25,12 @@ def merge_animals_tables(tables):
         animals = pd.concat(relevant)
         return animals
     return relevant[0]
+
 def prepare_filters(filters):
+    """
+    :param filters:  list of regular expressions
+    :return: string representing all matching groups
+    """
     filters = [f"({filter})" for filter in filters]
     all_filters = "|".join(filters)
     return all_filters
@@ -33,6 +39,7 @@ def grab_relevant_data(df):
     """
     gets an animal DF and strips all of the irrelevant characters
     from the 'Animal' (animal name) column.
+    :param df: dataframe of animals
     :return: pd.DataFrame, containing 'Animals' and 'Collateral adjectives'
     """
     # removes values which point to another value
@@ -52,11 +59,12 @@ def grab_relevant_data(df):
 def pretty_print(data):
     """
     For each collateral adjective, prints all animals.
+    :param data: DataFrame of relevant animals data
     :return: None
     """
     by_adjective = data.groupby(consts.COLLATERAL_ADJECTIVE_COL)
     for adjective, animals in by_adjective:
-        all_animals = ', '.join(animals["Animal"].tolist())
+        all_animals = ', '.join(animals[consts.ANIMAL_COL].tolist())
         print(f"{adjective}: {all_animals}")
 
 
