458f81249fdef50cf6c8b55ed49809ab8189d73b
hodayash <hodayash11@gmail.com>
2020-08-24 00:56:52 +0300

    readable code, same functionality

--- html_parser.py
+++ html_parser.py
@@ -2,59 +2,41 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-class HTMLTableParser:
-       
-        def parse_url(self, url):
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'lxml')
-            return [self.parse_html_table(table)\
-                    for table in soup.find_all('table')]  
-    
-        def parse_html_table(self, table):
-            n_columns = 0
-            n_rows=0
-            column_names = []
-    
-            # Find number of rows and columns
-            # we also find the column titles if we can
-            for row in table.find_all('tr'):
+def parse_url(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    return [parse_html_table(table) \
+            for table in soup.find_all('table')]  
+
+def parse_html_table(table):
+    """
+    finds the relevant tags for a table.
+    :param table: an html 'table' object of BeautifulSoup
+    :return: a DataFrame object representing the table.
+    """
+    parsed_table = []
+    column_names = []
+
+    #did not use list comprehension so the code will be readable
+    for row in table.find_all('tr'):
+        #grabs the table headers, only the first row containing it
+        th_tags = row.find_all('th')
+        if th_tags and not column_names:
+            #would let us create titles for the DataFrame
+            column_names = [th.get_text().strip() for th in th_tags]
+        else: 
+            td_tags = row.find_all('td')
+            row = [td.get_text().strip() for td in td_tags]
+            if row:
+                parsed_table.append(row)
+
+        #we assume the row width is consistent
+        table_width = len(column_names) if column_names else len(parsed_table[0])
+        table_height = len(parsed_table)
+
+        titles = column_names if column_names else range(0, table_width)
+
+        df = pd.DataFrame(parsed_table, columns=titles,
+                          index=range(0, table_height))
                 
-                # Determine the number of rows in the table
-                td_tags = row.find_all('td')
-                if len(td_tags) > 0:
-                    n_rows+=1
-                    if n_columns == 0:
-                        # Set the number of columns for our table
-                        n_columns = len(td_tags)
-                        
-                # Handle column names if we find them
-                th_tags = row.find_all('th') 
-                if len(th_tags) > 0 and len(column_names) == 0:
-                    for th in th_tags:
-                        column_names.append(th.get_text())
-    
-            # Safeguard on Column Titles
-            if len(column_names) > 0 and len(column_names) != n_columns:
-                raise Exception("Column titles do not match the number of columns")
-    
-            columns = column_names if len(column_names) > 0 else range(0,n_columns)
-            df = pd.DataFrame(columns = columns,
-                              index= range(0,n_rows))
-            row_marker = 0
-            for row in table.find_all('tr'):
-                column_marker = 0
-                columns = row.find_all('td')
-                for column in columns:
-                    df.iat[row_marker,column_marker] = column.get_text()
-                    column_marker += 1
-                if len(columns) > 0:
-                    row_marker += 1
-                    
-            # Convert to float if possible
-            for col in df:
-                try:
-                    df[col] = df[col].astype(float)
-                except ValueError:
-                    pass
-            
-            return df
+        return df
